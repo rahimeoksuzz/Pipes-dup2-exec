76aa29ee8371cee90d9bb4fa8cd5c626f047ecf1
rahimeoksuzz <61289562+rahimeoksuzz@users.noreply.github.com>
2020-05-20 23:50:37 +0300

    Add files via upload

---
+++ pipes-dup2-exec.c
@@ -0,0 +1,83 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char **argv)
+{
+	 /*int fd[2];
+    pipe(fd, O_CLOEXEC);  // Call with the O_CLOEXEC flag to prevent any commands from blocking */
+    
+	int fd[2];  /*okuma 0, yazma 1*/
+	pipe(fd);
+	pid_t pid = fork();
+	
+	if (pid == 0)		/*child*/
+	{
+		dup2(fd[1], 1);
+		close(fd[0]);
+		close(fd[1]);
+		execlp("ls", "ls", "-1", (char*)NULL);
+		exit(1);
+	}
+	else
+	{
+		pid = fork();
+		if (pid == 0)
+		{
+			dup2(fd[0], 0);
+			close(fd[1]);
+			close(fd[0]);
+			execlp("cut", "cut", "-f1", "-d.", (char *)NULL);
+			exit(-1);
+		}
+		else
+		{
+			pid = fork();
+			if (pid == 0)
+			{
+				dup2(fd[0], 0);
+				close(fd[1]);
+				close(fd[0]);
+				execlp("sort", "sort", (char*)NULL);
+				exit(-1);
+			}
+			else
+			{
+				pid = fork();
+				if (pid == 0)
+				{
+					dup2(fd[0], 0);
+					close(fd[1]);
+					close(fd[0]);
+					execlp("uniq", "uniq", (char*)NULL);
+					exit(-1);
+				}
+				else
+				{
+					pid = fork();
+					if (pid == 0)
+					{
+						dup2(fd[0], 0);
+						close(fd[1]);
+						close(fd[0]);
+						execlp("tee", "tee", "dirents", (char*)NULL);
+						exit(-1);
+					}
+					else
+					{
+						int status;
+						close(fd[0]);
+						close(fd[1]);
+						waitpid(pid, &status, 0);
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
+
